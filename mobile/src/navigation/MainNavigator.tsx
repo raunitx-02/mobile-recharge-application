2e4e9594aa1d154abf464a6a8bd68b1a1544d2e1
Raunit <rravenger7@gmail.com>
2026-06-28 15:26:47 +0530

    fix: HistoryScreen default export import mismatch in MainNavigator

--- mobile/src/navigation/MainNavigator.tsx
+++ mobile/src/navigation/MainNavigator.tsx
@@ -3,7 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/main/HomeScreen';
 import { RechargeScreen } from '../screens/main/RechargeScreen';
 import WalletScreen from '../screens/main/WalletScreen';
-import { HistoryScreen } from '../screens/main/HistoryScreen';
+import HistoryScreen from '../screens/main/HistoryScreen';
 import { ProfileScreen } from '../screens/profile/ProfileScreen';
 import { FloatingTabBar } from '../components/ui/FloatingTabBar';
 import { MainTabParamList } from './types';
